1d4b06e1ca3400072c44d18a89c97d82589fbdfb
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-08-24 18:06:29 -0700

    organizethe build folder

--- src/states/Preloader.js
+++ src/states/Preloader.js
@@ -14,12 +14,12 @@ Game.Preloader.prototype = {
         // this.load.setPreloaderSprite(this.preloadBar);
 
         //Load All Assets
-        this.load.image('bg2', 'bg2.jpg');
-        this.load.image('platform', 'platform.png');
-        this.load.image('battery', 'battery.png');
-        this.load.spritesheet('dude3', 'dude4_small.png', 71, 100); //width div frames, height 
-        this.load.tilemap('map', 'level2.csv', null, Phaser.Tilemap.CSV);
-        this.load.image('tiles', 'phase-2.png', 32, 32);
+        this.load.image('bg2', 'assets/bg2.jpg');
+        this.load.image('platform', 'assets/platform.png');
+        this.load.image('battery', 'assets/battery.png');
+        this.load.spritesheet('dude3', 'assets/dude4_small.png', 71, 100); //width div frames, height 
+        this.load.tilemap('map', 'assets/level2.csv', null, Phaser.Tilemap.CSV);
+        this.load.image('tiles', 'assets/phase-2.png', 32, 32);
     },
     create: function(){
         this.state.start('MainMenu');
