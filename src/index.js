1753c9a5f8b671b18f28cdff677c0693ffd0a394
Jamie Wang <sephona@gmail.com>
2017-08-25 10:46:06 -0400

    fixed merge conflict for window size

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 window.onload = function() {
-<<<<<<< HEAD
-
-	// var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
-
-	// function preload () {
-	// 	game.load.image('logo', '../phaser.png');
-	// }
-
-	// function create () {
-	// 	var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-	// 	logo.anchor.setTo(0.5, 0.5);
-	// }
-    let game = new Phaser.Game(800, 600, Phaser.AUTO, '');
-=======
     let game = new Phaser.Game(1600, 800, Phaser.AUTO, '');
->>>>>>> dev
     game.state.add('Boot', Game.Boot);
     game.state.add('Preloader', Game.Preloader);
     game.state.add('MainMenu', Game.MainMenu);
