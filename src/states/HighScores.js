49180a03c06dd3b20e50a50f228e863937daab08
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-08-24 22:38:53 -0700

    refactor High Scores

--- src/states/HighScores.js
+++ src/states/HighScores.js
@@ -2,23 +2,21 @@ Game.HighScores = function(game){
 
 }
 
-Game.HighScores.prototype = {create:createHighScores,
-                             createButton:createButton,
-                             createText:createText};
-
-function createHighScores(game){
-    game.add.sprite(0,0,'bg2');
-
-    this.createText(game, 'High Scores', 800/3, 50, '40px Arial', '#FFF', 'center');
-
-    this.createButton(game, 'Go Back to Menu', 125, 75,
-    200, 50, function(){
-        this.state.start('MainMenu');
-    });
-
-    let highScore = 'Name';
-    this.createText(game, highScore, 200, 150, '20px Arial', '#FFF', 'center');
-
-    highScore = 'Score';
-    this.createText(game, highScore, 500, 150, '20px Arial', '#FFF', 'center');
-}
+Game.HighScores.prototype = {
+    create: function(game) {
+        game.add.sprite(0,0,'bg2');
+    
+        createText(game, 'High Scores', 800/3, 50, '40px Arial', '#FFF', 'center');
+    
+        createButton(game, 'Go Back to Menu', 125, 75, 200, 50, 
+        () => {
+            this.state.start('MainMenu');
+        });
+    
+        let highScore = 'Name';
+        createText(game, highScore, 200, 150, '20px Arial', '#FFF', 'center');
+    
+        highScore = 'Score';
+        createText(game, highScore, 500, 150, '20px Arial', '#FFF', 'center');      
+    }
+};
