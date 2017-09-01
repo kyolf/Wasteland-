55174ca8012b823c3f17308cbe6e82a71132d41e
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-09-01 11:09:40 -0700

    changed victory screen text

--- src/states/Victory.js
+++ src/states/Victory.js
@@ -6,7 +6,7 @@ Game.Victory.prototype = {
     create: function(game) {
         this.bg = game.add.sprite(0,0,'victory');
         this.bg.scale.setTo(0.8,1);
-        createText(game, 'Victory', 615, 325, '100px Freckle Face', '#FFF');
+        createText(game, 'Congrats, You Escaped', 800, 325, '100px Freckle Face', '#FFF', 'center', 0.5, 0.5);
         createButton(game, 'Go Back to Menu', 175, 75,
         275, 60, () => {
             this.state.start('MainMenu');
