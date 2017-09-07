14d8249d1273eaa4cde5fa3c90e1df80dbb1a5e9
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-09-07 12:24:53 -0700

    create comments for gameover

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -3,9 +3,11 @@ Game.GameOver = function(game) {
 
 Game.GameOver.prototype = {
     create: function(game) {
+        //Making sure there is no memory leak and restarting music
         game.global.shadowTexture.destroy();
         game.global.menuMusic = true;
 
+        //prompt for high scores
         fetchTopScores()
         .then(highScores => {
             if(highScores.length >= 10) {
@@ -22,13 +24,11 @@ Game.GameOver.prototype = {
             }
         });
 
+        //Game Over Background
         let background = game.add.sprite(0, 0, 'gameover');
         background.scale.setTo(0.5, 0.5);
 
-        // let grim = game.add.sprite(400, 275, 'grim');
-        // grim.anchor.setTo(0.4, 0.4);
-        // grim.scale.setTo(0.5, 0.5);
-
+        //Player Dead Animations
         this.playerDead = game.add.sprite(265, 275, 'faint');
         this.playerDead.animations.add('fainting', [5, 0, 2, 1, 3, 4, 5], 10, true);
         this.playerDead.animations.play('fainting');
@@ -36,9 +36,9 @@ Game.GameOver.prototype = {
         this.playerDead.animations.currentAnim.speed = 4;
         this.playerDead.events.onAnimationLoop.add(function(){return this.playerDead.animations.stop();}, this);
         
-        
         createText(game, 'Game Over', 175, 125, '150px murderFont', '#FFF');
-        // createText(game, 'YOUR SOUL IS MINE!', 425, 350, '80px murderFont', '#FFF','center', 0.5, 0.5);
+        
+        //Image Button + Piglet Arrows
         createImageButton(game, 'Go Back to Menu', 250, 50, 100, 50);
         
         this.arrow = game.add.sprite(80, 50, 'piglet');
@@ -46,9 +46,10 @@ Game.GameOver.prototype = {
         this.arrow.canMove = true;
         this.arrow.animations.add('right', [2,3], 5, true);
     },
+    //Going Back to Main Menu Purposes
     update: function(game) {
         this.arrow.animations.play('right');
-        if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) {
             game.state.start('MainMenu');
         }
     }
