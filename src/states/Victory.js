9e51d4d98611661f199e9c80da001c1479908e24
Jamie Wang <sephona@gmail.com>
2017-09-06 23:27:02 -0400

    adjusted spacing for victory page text and also took out button on victory page

--- src/states/Victory.js
+++ src/states/Victory.js
@@ -29,10 +29,6 @@ Game.Victory.prototype = {
 
         this.bg = game.add.sprite(0,0,'victory');
         this.bg.scale.setTo(0.8,1);
-        createText(game, 'Congrats, You Escaped', 350, 325, '100px murderFont', '#FFF', 'center', 0.5, 0.5);
-        createButton(game, 'Go Back to Menu', 100, 50,
-        175, 50, () => {
-            this.state.start('MainMenu');
-        });
+        createText(game, 'Congrats, You Escaped', 400, 275, '100px murderFont', '#FFF', 'center', 0.5, 0.5);
     }
 };
